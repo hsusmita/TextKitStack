92eaf34a98ddb040ddd498c796012a22fa0d81d6
hsusmita <susmita.horrow@gmail.com>
2015-09-01 13:19:37 +0530

    rearranging file structure

---
+++ TextKitStack/TextKitStack/Source/TextKitStack.h
@@ -0,0 +1,13 @@
+//
+//  TextKitStack.h
+//  TextKitStack
+//
+//  Created by hsusmita on 01/09/15.
+//  Copyright (c) 2015 hsusmita.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TextKitStack : NSObject
+
+@end
---
+++ TextKitStack/TextKitStack/Source/TextKitStack.m
@@ -0,0 +1,13 @@
+//
+//  TextKitStack.m
+//  TextKitStack
+//
+//  Created by hsusmita on 01/09/15.
+//  Copyright (c) 2015 hsusmita.com. All rights reserved.
+//
+
+#import "TextKitStack.h"
+
+@implementation TextKitStack
+
+@end
