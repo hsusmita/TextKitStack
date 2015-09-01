a7adb325ff6c6435ee3017b33365da8149cb8ea6
hsusmita <susmita.horrow@gmail.com>
2015-09-01 13:04:08 +0530

    Initial Project added

---
+++ TextKitStack/TextKitStack/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextKitStack
+//
+//  Created by hsusmita on 01/09/15.
+//  Copyright (c) 2015 hsusmita.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
